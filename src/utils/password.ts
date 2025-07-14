ce6661a55a446b4089c77465a9356336ce045666
Sérgio Timoteo <74915883+sergiotim@users.noreply.github.com>
2025-07-14 12:10:25 -0300

    Revert "implementando o login"
    This reverts commit 6ce7a234e20f56cc54278022cda0a6f3caaa061a.

--- src/utils/password.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcrypt"
-
-const SALT_ROUNDS = 10
-
-export async function saltAndHashPassword(password:any) : Promise<string>{
-    return await bcrypt.hash(password,SALT_ROUNDS)
-    
-}
